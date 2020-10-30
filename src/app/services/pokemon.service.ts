c934eaf06c7377dbb4924cfb1cb5469b1c18e47e
Josefa Herrera Sepúlveda <Josefa@MacBook-Pro.local>
2020-10-30 12:26:50 -0300

    cambio secreto

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PokemonService {
 
-  baseUrl: string = environment.baseUrl;
+  baseUrl = 'https://pokeapi.co/api/v2';
 
   constructor(private http: HttpClient) { }
 
